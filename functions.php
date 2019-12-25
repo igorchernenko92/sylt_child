a776bfd70c14fb432e00ce6f88cf1b18f589fd86
Igor Chernenko <igorchernenko92@gmail.com>
2019-12-25 21:09:48 +0200

    ADD: edit request and ADD: multiselect field

--- functions.php
+++ functions.php
@@ -24,20 +24,28 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 // END ENQUEUE PARENT ACTION
 
 
-//add_action( 'init', 'action_function_name_11' );
-//function action_function_name_11() {
-//    $result = '';
-//
-//
-//
-//        $query_args['meta_query']['offer'] = array(
-//            'key'     => '_price_offer',
-//            'value'   => $args['offer'],
-//            'compare' => 'IN'
-//        );
-//
-//    $result = new WP_Query( $query_args );
-//
-//    add_filter( 'wpsight_get_listings', $result, $query_args, $args );
-//
-//}
+function edit_listing_request($query_args, $args) {
+
+    $query_args['meta_query']['offer'] = array(
+        'key'     => '_price_offer',
+        'value'   =>  htmlspecialchars( $_GET['offer' ]),
+        'compare' => 'IN'
+    );
+    $result = new WP_Query( $query_args );
+
+    return $query_args;
+}
+
+add_filter( 'wpsight_get_listings_query_args', 'edit_listing_request', 10 , 3 );
+
+
+function edit_default_fields($fields_default) {
+
+    $fields_default['location']['type'] = 'multiselect';
+    $fields_default['listing-type']['type'] = 'multiselect';
+
+
+    return wpsight_sort_array_by_priority( $fields_default );
+}
+
+add_filter( 'wpsight_get_search_fields', 'edit_default_fields');
