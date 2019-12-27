72c926a73f00118235a31197c6265fe59fbc92b4
Igor Chernenko <igorchernenko92@gmail.com>
2019-12-27 19:56:46 +0200

    ADD: search background image

--- wpcasa/search-form.php
+++ wpcasa/search-form.php
@@ -1,5 +1,5 @@
 <form method="get"<?php echo $args['id']; ?> action="<?php echo esc_url( $args['action'] ); ?>" class="<?php echo sanitize_html_class( $args['class'] ); ?> <?php echo sanitize_html_class( $args['orientation'] ); ?>">
-	
+
 	<div class="listings-search-default">
 		<div class="row">
 			<?php echo $search_default; ?>
@@ -8,7 +8,7 @@
             <?php endif; ?>
 		</div>
 	</div><!-- .listings-search-default -->
-	
+
 
 
 		<div class="listings-search-advanced">
@@ -16,18 +16,18 @@
 <!--				--><?php //echo $search_advanced; ?>
 			</div>
 		</div><!-- .listings-search-advanced -->
-			
+
 		<?php // Display advanced search toggle button ?>
 <!--		--><?php //echo wp_kses_post( $args['advanced'] ); ?>
-	
-	<?php if( $args['reset'] !== false ) : ?>	
+
+	<?php if( $args['reset'] !== false ) : ?>
 		<?php // Display reset search button ?>
-		<?php echo wp_kses_post( $args['reset'] ); ?>			
+		<?php echo wp_kses_post( $args['reset'] ); ?>
 	<?php endif; ?>
-	
+
 	<?php if( isset( $_GET['page_id'] ) ) : ?>
 		<?php // Add current page_id to GET parameters if permalinks are not pretty ?>
 		<input name="page_id" type="hidden" value="<?php echo absint( $_GET['page_id'] ); ?>" />
 	<?php endif; ?>
-		
+
 </form><!-- .<?php echo sanitize_html_class( $args['class'] ); ?> -->
