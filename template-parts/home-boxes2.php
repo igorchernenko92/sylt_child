10e2aaedfc60501b0a565784fedc3e1c5db588fe
Igor Chernenko <igorchernenko92@gmail.com>
2019-12-27 18:39:17 +0200

    ADD: home boxes templates

--- template-parts/home-boxes2.php
+++ template-parts/home-boxes2.php
@@ -1,20 +1,18 @@
 <?php
 /**
- * Home boxes template
+ * Home boxes2 template
  *
  * @package WPCasa Sylt Child
  */
 
 //if( $display ) : ?>
 
-<?php $boxes = get_post_meta( get_the_id(), '_boxes', true ); ?>
+<?php $boxes = get_post_meta( get_the_id(), '_boxes2', true ); ?>
 <?php if( $boxes ) : ?>
     <?php foreach( $boxes as $key => $box ) : ?>
             <?php
                 $title	= isset( $box['_title'] )  ? $box['_title'] : false;
-                $description	= isset( $box['_description'] )  ? $box['_description'] : false;
                 $url	= isset( $box['_url'] )  ? $box['_url'] : false;
-                $button	= isset( $box['_button'] )  ? $box['_button'] : false;
                 $img	= isset( $box['_image'] )  ? $box['_image'] : false;
             ?>
     <?php endforeach; ?>
