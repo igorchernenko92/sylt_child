1d7186b3da49103753524580f18c81388b185c73
Igor Chernenko <igorchernenko92@gmail.com>
2020-01-31 17:42:06 +0200

    small refactor

--- wpcasa/listing-single-details.php
+++ wpcasa/listing-single-details.php
@@ -6,10 +6,10 @@ global $listing; ?>
 
 <div class="wpsight-listing-section wpsight-listing-section-details">
 	
-	<?php do_action( 'wpsight_listing_single_details_before', $listing->ID ); ?>
+	<?php do_action( 'wpsight_listing_single_details_before', get_the_ID() ); ?>
 
-    <?php wpsight_listing_summary( $listing->ID, array( 'details_1', 'details_2', 'details_3', 'details_4' ) ) ?>
+    <?php wpsight_listing_summary( get_the_ID(), array( 'details_1', 'details_2', 'details_3', 'details_4' ) ) ?>
 	
-	<?php do_action( 'wpsight_listing_single_details_after', $listing->ID ); ?>
+	<?php do_action( 'wpsight_listing_single_details_after', get_the_ID()); ?>
 
 </div><!-- .wpsight-listing-section-details -->
