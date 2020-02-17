c3096fb546e16e966a5f79849576b2afbf08854f
Igor Chernenko <igorchernenko92@gmail.com>
2020-02-17 11:16:24 +0200

    DELETE: ion slider

--- functions.php
+++ functions.php
@@ -60,7 +60,7 @@ if ( ! function_exists('ccc_theme_enqueue') ) :
     );
 
     // enqueue CSS for front page
-    wp_enqueue_style( 'chld_thm_ion_range_slider_css', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/ion-range-slider/ion-range-slider.min.css');
+//    wp_enqueue_style( 'chld_thm_ion_range_slider_css', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/ion-range-slider/ion-range-slider.min.css');
     wp_enqueue_style( 'chld_thm_multiselect_css', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/multiple-select/multiple-select.min.css');
     wp_enqueue_style( 'chld_thm_select2_css', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/select2/select2.min.css');
     wp_enqueue_style( 'chld_thm_swiper', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/swiper/swiper.min.css');
@@ -69,7 +69,7 @@ if ( ! function_exists('ccc_theme_enqueue') ) :
     wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'skel-main','skel-grid' ) );
 
     // enqueue JS for front page
-    wp_enqueue_script( 'chld_thm_ion_range_slider_js', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/ion-range-slider/ion-range-slider.min.js' );
+//    wp_enqueue_script( 'chld_thm_ion_range_slider_js', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/ion-range-slider/ion-range-slider.min.js' );
     wp_enqueue_script( 'chld_thm_multiselect_js', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/multiple-select/multiple-select.min.js' );
     wp_enqueue_script( 'chld_thm_select2_js', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/select2/select2.min.js' );
     wp_enqueue_script( 'chld_thm_swiper', trailingslashit( get_stylesheet_directory_uri() ) . 'vendor/swiper/swiper.min.js' );
