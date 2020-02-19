484d42e16e55bd6174da878ac73c7dfe6fba96b2
Igor Chernenko <igorchernenko92@gmail.com>
2020-02-19 16:46:30 +0200

    ADD: hide empty true

--- wpcasa/search-fields/field-multiselect.php
+++ wpcasa/search-fields/field-multiselect.php
@@ -1,7 +1,7 @@
 <?php if( isset( $fields[$field]['data'] ) && is_array( $fields[$field]['data'] ) ) : ?>
     <div data-select-all="true" class="listings-search-field listings-search-field-<?php echo esc_attr( $fields[$field]['type'] ); ?> listings-search-field-<?php echo esc_attr( $field ); ?> <?php echo esc_attr( $class ); ?>">
         <?php
-            $terms = get_terms( ['taxonomy' => $field, 'hide_empty' => false ] );
+            $terms = get_terms( ['taxonomy' => $field, 'hide_empty' => true ] );
             $output = get_terms_hierarchical($terms);
         ?>
         <select class="multiselect" name="<?php echo $field ?>[]" placeholder="<?php echo $fields[$field]['data']['show_option_none'] ?>" multiple="multiple">
